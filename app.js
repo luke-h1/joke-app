7ac932fdabe9ce72d06ce992af9a026aba157d4c
luke-h1 <luke.howsam@yahoo.com>
2020-07-29 15:49:23 +0100

    refactor. still gettng back json data

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-const form = document.querySelector('#myForm');
+/* 
+This shit works.... 
+*/
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const API_URL =
-        'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+// const form = document.querySelector('#myForm');
 
-    fetch(API_URL)
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const API_URL =
+//         'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+
+//     fetch(API_URL)
+//         .then((res) => res.json())
+//         .then((res) => {
+//             console.log(res);
+//             const output = document.querySelector('#output');
+//             j
+//         });
+// });
+
+/* this shit probably won't work....
+ */
+
+const API_URL =
+    'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+
+// variables
+const output = document.querySelector('#output');
+const loadingElement = document.querySelector('.loading');
+
+// set loading element to not display
+loadingElement.style.display = 'none';
+
+// async function and await response from API
+async function getData() {
+    loadingElement.style.display = ''; 
+    await fetch(API_URL)
         .then((res) => res.json())
         .then((res) => {
+            loadingElement.style.display = 'none'; 
             console.log(res);
-      
+            
         });
-});
+}
+
+getData()
+
